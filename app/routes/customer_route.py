b8d151e0d858b9316e388b25c8b2f6d4c970900e
pgvda <dilshanariyarathna1999@gmail.com>
2024-07-02 11:01:00 +0530

    finalBAckend

--- app/routes/customer_route.py
+++ app/routes/customer_route.py
@@ -4,13 +4,13 @@ from app.extension import db
 
 customer_bp = Blueprint('customers', __name__)
 
-@customer_bp.route('/', methods=['GET'])
+@customer_bp.route('/show/customer', methods=['GET'])
 def get_customers():
     customers = Customer.query.all()
     result = [{'id': customer.id, 'name': customer.name, 'email': customer.email} for customer in customers]
     return jsonify(result)
 
-@customer_bp.route('/', methods=['POST'])
+@customer_bp.route('/add/customer', methods=['POST'])
 def add_customer():
     data = request.get_json()
     new_customer = Customer(name=data['name'], email=data['email'])
